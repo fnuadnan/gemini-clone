0a5e82ab8d4cd32f6e7a95e56c0182f4b8840173
Fnu Adnan <129648022+fnuadnan@users.noreply.github.com>
2024-06-05 22:05:38 -0500

    Add sidebar component with styles and functionality

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,49 @@
+import { useState } from "react";
+import { assets } from "../../assets/assets";
 import "./Sidebar.css";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const [extended, setExtended] = useState(false);
+
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <img
+          onClick={() => setExtended(!extended)}
+          className="menu"
+          src={assets.menu_icon}
+          alt=""
+        />
+        <div className="new-chat">
+          <img src={assets.plus_icon} alt="" />
+          {extended ? <p>New Chat</p> : null}
+        </div>
+        {extended ? (
+          <div className="recent">
+            <p className="recent-title">Recent</p>
+            <div className="recent-entry">
+              <img src={assets.message_icon} alt="" />
+              <p>What is react ...</p>
+            </div>
+          </div>
+        ) : null}
+      </div>
+      <div className="bottom">
+        <div className="bottom-item recent-entry">
+          <img src={assets.question_icon} alt="" />
+          {extended ? <p>Help</p> : null}
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.history_icon} alt="" />
+          {extended ? <p>Activity</p> : null}
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.setting_icon} alt="" />
+          {extended ? <p>Settings</p> : null}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
